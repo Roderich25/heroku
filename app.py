cb90d1b618f45c66f0fa4b43b821f5569dec505e
Roderich25 <rodrigoavilasolis@gmail.com>
2019-06-12 14:03:10 -0500

    heroku test

---
+++ app.py
@@ -0,0 +1,31 @@
+import time
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/posts", methods=["POST"])
+def posts():
+    # start and end point for posts
+    start = int(request.form.get("start") or 0)
+    end = int(request.form.get("end") or (start + 9))
+
+    # list of posts
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+
+    # simulate delay of response
+    time.sleep(1)
+
+    # return list of posts
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(port=9130, debug=True)
